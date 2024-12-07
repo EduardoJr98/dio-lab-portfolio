ad0646c1a7417349132d2b62f64d061366ab484c
EduardoJr98 <153054937+EduardoJr98@users.noreply.github.com>
2024-12-07 16:49:07 -0300

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,31 @@
 const toggleTheme = document.getElementById("toggleTheme");
-const rootHtml = document.documentElement
+const rootHtml = document.documentElement;
 const accordionHeaders = document.querySelectorAll(".accordion__header");
 const menuLinks = document.querySelectorAll(".menu__link");
 
-function changeTheme(){
+function changeTheme() {
   const currentTheme = rootHtml.getAttribute("data-theme");
 
-  currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark")
+  const newTheme = currentTheme === "dark" ? "light" : "dark";
+  rootHtml.setAttribute("data-theme", newTheme);
 
-  toggleTheme.classList.toggle("bi-sun")
-  toggleTheme.classList.toggle("bi-moon-stars")
+  toggleTheme.classList.toggle("bi-sun");
+  toggleTheme.classList.toggle("bi-moon-stars");
+
+  localStorage.setItem("theme", newTheme);
+}
+
+function loadTheme() {
+  const savedTheme = localStorage.getItem("theme") || "light"; // Tema padrão: "light"
+  rootHtml.setAttribute("data-theme", savedTheme);
+
+  if (savedTheme === "dark") {
+    toggleTheme.classList.add("bi-moon-stars");
+    toggleTheme.classList.remove("bi-sun");
+  } else {
+    toggleTheme.classList.add("bi-sun");
+    toggleTheme.classList.remove("bi-moon-stars");
+  }
 }
 
 toggleTheme.addEventListener("click", changeTheme);
@@ -20,12 +36,14 @@ accordionHeaders.forEach(header => {
     const accordionActive = accordionItem.classList.contains("active");
 
     accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
-  })
-})
+  });
+});
 
 menuLinks.forEach(item => {
   item.addEventListener("click", () => {
     menuLinks.forEach(i => i.classList.remove("active"));
     item.classList.add("active");
-  })
-})
+  });
+});
+
+loadTheme();
